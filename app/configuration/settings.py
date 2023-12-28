bac25bf45fcb45774d46df9a51889c0ea756248e
diego-silva-entrounaconta <diego@entrounaconta.com>
2023-12-28 15:21:57 -0300

    add arguments

--- app/configuration/settings.py
+++ app/configuration/settings.py
@@ -2,6 +2,6 @@ arguments = [
     "--start-maximized",
     "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.0.0 Safari/537.36",
     "--headless",
-    # "--disable-dev-shm-usage",
-    # "--no-sandbox",
+    "--disable-dev-shm-usage",
+    "--no-sandbox",
 ]
